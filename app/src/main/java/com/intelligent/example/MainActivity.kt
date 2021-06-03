7997995c5fb8263ee940883b6b6a2cb748426d5c
mpountou <giannis.boudou@gmail.com>
2021-06-03 14:17:02 +0300

    screenshot

--- app/src/main/java/com/intelligent/example/MainActivity.kt
+++ app/src/main/java/com/intelligent/example/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var dialogManager = DialogManager(this)
-
         dialogManager.show()
     }
 }
